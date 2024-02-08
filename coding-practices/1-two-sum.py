4dfa2782b91b6008e4ac43f02a2eec32f583e216
gabrielhso <gabriel.onishi@outlook.com>
2024-02-08 10:46:04 -0300

    Primeiro problema de coding practice

---
+++ coding-practices/1-two-sum.py
@@ -0,0 +1,6 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        for i, num_i in enumerate(nums):
+            for j, num_j in enumerate(nums):
+                if num_i + num_j == target and i != j: return [i, j]

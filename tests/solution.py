012c24f6d867f08991db4017659ae6b85345f574
gabriel.onishi <gabriel.onishi@outlook.com>
2024-02-27 19:43:59 -0300

    Implements first version of interview problem 3b (with problems

--- tests/solution.py
+++ tests/solution.py
@@ -1,68 +1,69 @@
 '''
-Problem 2.7 - Cracking the Coding Interview
+Problem 3.5 - Cracking the Coding Interview
 
-Given two linked lists, determine if the two lists intersect. Return the intersection node.
-Definitions
+Write a program to sort a stack such that the smallest items are on the top. The stack supports the following operations: push, pop, peek, and is_empty.
 
-    Intersection: the intersection of two linked lists is based on reference, not value. In other words, even if two lists have nodes with the same value, that doesn't mean they intersect.
+Constraints
 
-Additional information
-
-    The lists are singly linked, i.e. there is no pointer to the previous node.
-
-Hints
-
-     - Examples are useful. Draw two linked lists with an intersection and two equivalent lists that don't intersect;
-     - Start by checking if there is an intersection before looking for the exact place it occurs;
-     - Note that if two lists intersect, they will always share the last node. Once they intersect, all following nodes are shared;
-     - After you have determined that the two lists intersect, you have to find the intersection node. If the two lists had the exact same length, how would you solve the problem?
-     - If the two lists had the same length, you could traverse them in parallel until you find a shared node. Now, how can you adapt this idea to lists with different lengths?
-     - Is the length difference useful for anything?
+    - You may not copy the elements into any other data structure (such as an array);
+    - You can use an additional temporary stack, but not two.
 
 '''
 
-class Node:
-    def __init__(self, value, nxt=None):
-        self.value = value
-        self.next = nxt    
+class Stack:
+    def __init__(self):
+        self._items = []
 
-def find_intersection(head1: Node, head2: Node) -> Node:
+    def push(self, value):
+        self._items.append(value)
 
-    if head1 is None or head2 is None:
-        return None
+    def pop(self):
+        return self._items.pop()
 
-    size1 = 1    
-    tail1 = head1
-    
-    size2 = 1
-    tail2 = head2
-    
-    while tail1.next is not None:
-        tail1 = tail1.next
-        size1 += 1
-    
-    while tail2.next is not None:
-        tail2 = tail2.next
-        size2 += 1
-        
-    if tail1 != tail2:
-        return None
+    def peek(self):
+        return self._items[-1]
 
-    size_diff = size1 - size2
-    
-    node1 = head1
-    node2 = head2
-    
-    for i in range(abs(size_diff)):
-        if size_diff > 0:
-            node1 = node1.next
+    def is_empty(self):
+        return len(self) == 0
+
+    def __len__(self):
+        return len(self._items)
+
+
+def sort_stack(stack: Stack) -> None:
+    '''
+    Ideia: utilizar uma pilha de suporte e fazer o sorting contrário
+    (maiores no topo) enquanto monta ela
+    '''
+
+    if stack.is_empty(): return
+
+    # Adiciona primeiro elemento em suuport
+    support = Stack()
+    support.push(stack.pop())
+
+    while not stack.is_empty():
+        element = stack.pop()
+
+        # Se o elemento do topo for menor, colocar o maior em cima
+        if support.peek() < element:
+            support.push(element)
+        # Caso contrário, ir desmontando a de suporte até encontrar
+        # elemento menor
         else:
-            node2 = node2.next
-    
-    while node1 is not None or node2 is not None:
-        if node1 == node2:
-            return node1
-        node1 = node1.next
-        node2 = node2.next
-    
-    return None
+            i = 0
+            while True:
+                stack.push(support.pop())
+                i += 1
+                
+                if support.peek() < element:
+                    support.push(element)
+                    break
+            
+            for _ in range(i):
+                support.push(stack.pop())
+
+    while not support.is_empty():
+        stack.push(support.pop())
+
+            
--- tests/test_solution.py
+++ tests/test_solution.py
@@ -1,87 +1,70 @@
-from solution import find_intersection
+from solution import Stack, sort_stack
 
 
-class Node:
-    def __init__(self, value, nxt=None):
-        self.value = value
-        self.next = nxt
+def _check(values):
+    stack = Stack()
+    stack._items = [v for v in values]
+    sort_stack(stack)
+
+    msg = "Didn't work for the stack: {values}"
+    assert stack._items == sorted(values, reverse=True), msg
 
 
-def make_list(l: list) -> list[Node]:
-    nodes = [Node(v) for v in l]
-    for node, next in zip(nodes[:-1], nodes[1:]):
-        node.next = next
-    return nodes
+def test_1():
+    _check([])
 
 
-def strlist(node: Node) -> str:
-    s = []
-    while node:
-        s.append(str(node.value))
-        node = node.next
-    return '->'.join(s) or 'Empty'
+def test_2():
+    _check([1])
 
 
-def check(s1, s2, intersection):
-    ans = find_intersection(s1, s2)
-    msg = f"Didn't work for lists '{strlist(s1)}' and '{strlist(s2)}'"
-    assert ans == intersection, msg
+def test_3():
+    _check(list(range(1, 11)))
 
 
-def check_both(s1, s2, intersection):
-    check(s1, s2, intersection)
-    check(s2, s1, intersection)
+def test_4():
+    _check(list(range(10, 0, -1)))
 
 
-def test_example1():
-    l1 = make_list([4, 7, 1, 9, 4, 5, 3])
-    l2 = make_list([8, 2])
-    l2[-1].next = l1[4]
-    check_both(l1[0], l2[0], l1[4])
+def test_5():
+    values = []
+    for i in range(1, 11):
+        values.append(i)
+        values.append(10-i+1)
+    _check(values)
 
 
-def test_example2():
-    l1 = make_list([4, 7, 1, 9, 4, 5, 3])
-    l2 = make_list([8, 2, 4, 5, 3])
-    check_both(l1[0], l2[0], None)
+def test_6():
+    values = []
+    for i in range(1, 11):
+        for _ in range(5):
+            values.append(i)
+        for _ in range(5):
+            values.append(10-i+1)
+    _check(values)
 
 
-def test_none():
-    check_both(None, make_list([1, 2, 3, 4])[0], None)
+def test_7():
+    _check([1, 1, 1, 1, 1, 1, 1])
 
 
-def test_double_none():
-    check_both(None, None, None)
+def test_8():
+    _check([-1, -1, -1, -1, -1, -1, -1])
 
 
-def test_same_numbers():
-    check_both(make_list(range(1, 10))[0], make_list(range(1, 10))[0], None)
+def test_9():
+    values = []
+    for i in range(1, 11):
+        values.append(-(10-i+1))
+        values.append(-i)
+    _check(values)
 
 
-def test_same_list():
-    l = make_list(range(1, 10))
-    check_both(l[0], l[0], l[0])
-
-
-def test_in_last():
-    l1 = make_list(range(1, 15))
-    l2 = make_list(range(15, 25))
-    last = Node(30)
-    l1[-1].next = last
-    l2[-1].next = last
-    check_both(l1[0], l2[0], last)
-
-
-def test_second():
-    l = make_list(range(1, 15))
-    n1 = Node(16, l[0])
-    n2 = Node(16, l[0])
-    check_both(n1, n2, l[0])
-
-
-def test_one_empty():
-    check_both(Node(1), None, None)
-
-
-def test_one_one():
-    check_both(Node(1), Node(1), None)
+def test_10():
+    values = []
+    for i in range(1, 11):
+        for _ in range(5):
+            values.append(-(10-i+1))
+        for _ in range(5):
+            values.append(-i)
+    _check(values)

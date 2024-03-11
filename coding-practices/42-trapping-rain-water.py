fb265c77281cbd5486ae0f6eece6a07bc2e1776f
gabriel.onishi <gabriel.onishi@outlook.com>
2024-03-11 16:08:05 -0300

    Solves exercise 01

--- coding-practices/42-trapping-rain-water.py
+++ coding-practices/42-trapping-rain-water.py
@@ -3,27 +3,24 @@ from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        volume = 0
-        borders = dict()
-
-        for h in height:
-            if h > 0 and h not in borders:
-                borders[h] = 0
-            for border in borders:
-                if h < border:
-                    borders[border] += border - h
-                else:
-                    this_vol = borders[border]
-                    for b in borders:
-                        if b > border:
-                            borders[b] -= this_vol
-                    borders[border] = 0
-                    volume += this_vol
+        '''
+                    X
+          X         X
+          X     X   X
+          X X   X X X
+        X X X   X X X
+        --------------
+        0 1 2 3 4 5 6
+        '''
         
-        print(volume)
-        return(volume)
-                    
+        l_wall, r_wall = 0
+        n = len(height)
+        max_left, max_left = [0 for i in range(len(height))]
+        
+        for 
+        
+                                     
 if __name__ == '__main__':
     test = Solution()
-    height = [0,1,0,2,1,0,1,3,2,1,2,1]
+    height = [4,2,0,3,2,5]
     test.trap(height=height)

dab37a32fb6c77995ecf11f4deb5bac83c5b5b47
gabriel.onishi <gabriel.onishi@outlook.com>
2024-03-20 11:28:43 -0300

    Fix problema no ex 5b

--- mock_interviews/05/5_b.py
+++ mock_interviews/05/5_b.py
@@ -8,10 +8,9 @@ Write a program to swap odd and even bits in an integer with as few instructions
 '''
 
 def swap_odd_even_bits(x: int) -> int:
-    right_shift = x >> 1
-    left_shift = x << 1
+    even_bits = x & 0b10101010101010101010101010101010
+    odd_bits = x & 0b01010101010101010101010101010101
+    even_bits >>= 1
+    odd_bits <<= 1
     
-    odd_bit_mask = 0x01010101010101010101010101010101
-    even_bit_mask = 0x10101010101010101010101010101010
-    
-    return (right_shift & odd_bit_mask) + (left_shift & even_bit_mask)
+    return even_bits | odd_bits
